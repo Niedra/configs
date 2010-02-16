4880c1e7fb1087921ba4b5f9d3a3b1a3c9d28923
root <root@box.localdomain>
2010-02-16 15:04:01 +0200

    configs

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -354,4 +354,4 @@ client.add_signal("unfocus", function(c) c.border_color = beautiful.border_norma
 -- }}}
 
 -- Autostart
-awful.util.spawn_with_shell("nitrogen --restore")
+-- awful.util.spawn_with_shell("nitrogen --restore")
